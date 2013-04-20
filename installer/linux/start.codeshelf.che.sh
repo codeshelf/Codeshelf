4fc9457f8651607f821fb74e618e072e47a597d1
jeffw <jeffw>
2013-04-20 00:15:07 +0000

    Improve CHE build process

---
+++ installer/linux/start.codeshelf.che.sh
@@ -0,0 +1,1 @@
+/opt/java/jdk1.7.0_21/bin/java -Djava.library.path=/home/pi/appjar/lib/native/arm -Dcodeshelf.networknum=1 -Dguid=00000002 -Dcontroller.ipaddr=10.47.47.49 -jar /home/pi/appjar/che.codeshelf.jar
--- installer/linux/start.sh
+++
@@ -1,1 +0,0 @@
-/opt/java/jdk1.7.0_21/bin/java -Djava.library.path=/home/pi/appjar/lib/native/arm -Dcodeshelf.networknum=1 -jar appjar/che.codeshelf.jar
