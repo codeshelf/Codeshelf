f6647db34446224fd9d5cadcbb32b872cc6ff658
jeffw <jeffw>
2013-04-23 01:37:39 +0000

    Support to run protos on rpi

---
+++ installer/linux/start.codeshelf.aisle.sh
@@ -0,0 +1,1 @@
+/opt/java/jdk1.7.0_21/bin/java -Djava.library.path=/home/pi/appjar/lib/native/arm -Dconfig.properties=/home/pi/appjar/aisle.config.properties -jar /home/pi/appjar/aisle.codeshelf.jar
