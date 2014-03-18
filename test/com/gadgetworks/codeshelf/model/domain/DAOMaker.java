fd6c416ab56d52d3095fb69bd1891cbf32e15d0c
Paul Monteiro <paul.monteiro@codeshelf.com>
2014-03-17 23:12:39 -0700

    Support for complex objects in method requests.  So that new paths could be saved.

--- test/com/gadgetworks/codeshelf/model/domain/DAOMaker.java
+++ test/com/gadgetworks/codeshelf/model/domain/DAOMaker.java
@@ -19,25 +19,25 @@ public class DAOMaker {
 	public DAOMaker(ISchemaManager mSchemaManager) {
 		Organization.DAO = new OrganizationDao(mSchemaManager);
 		Facility.DAO = new FacilityDao(mSchemaManager);
-		
+
 	}
-	
+
 	public static final Property<Organization,String> organizationId = newProperty();
-	
+
 	public static final Property<Facility,String> facilityId = newProperty();
 	public static final Property<Facility,Organization> organization  = newProperty();
-	
+
 	public final Instantiator<Organization> TestOrganization = new Instantiator<Organization>() {
 	    public Organization instantiate(PropertyLookup<Organization> lookup) {
 
-	    	
+
 	    	Organization organization = new Organization();
 	    	organization.setOrganizationId(lookup.valueOf(organizationId, RandomStringUtils.randomAlphanumeric(5)));
 			Organization.DAO.store(organization);
 	        return organization;
 	    }
 	};
-	
+
 	public final Instantiator<Facility> TestFacility = new Instantiator<Facility>() {
 	    public Facility instantiate(PropertyLookup<Facility> lookup) {
 	        Facility facility = new Facility(new Point(PositionTypeEnum.GPS, 0.0d, 0.0d, 0.0d));
@@ -47,5 +47,5 @@ public class DAOMaker {
 	        return facility;
 	    }
 	};
-	
+
 }
