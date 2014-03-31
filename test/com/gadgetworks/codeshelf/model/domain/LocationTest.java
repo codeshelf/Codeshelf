ae151102afa5a8a4cd9643dc6223568a9f3dead8
Jeff Williams <jeff.williams@codeshelf.com>
2014-03-30 23:17:18 -0700

    * Add unit tests

---
+++ test/com/gadgetworks/codeshelf/model/domain/LocationTest.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ *  CodeShelf
+ *  Copyright (c) 2005-2012, Jeffrey B. Williams, All rights reserved
+ *  $Id: OrderGroupTest.java,v 1.4 2013/07/22 04:30:36 jeffw Exp $
+ *******************************************************************************/
+package com.gadgetworks.codeshelf.model.domain;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author jeffw
+ *
+ */
+public class LocationTest extends DomainTestABC {
+
+	@Test
+	public final void getLocationIdToParentLevel() {
+
+		Facility facility = createFacilityWithOutboundOrders("O-LOC.1");
+		
+		ISubLocation<?> aisle = facility.findLocationById("A1");
+		ISubLocation<?> bay = aisle.findLocationById("B1");
+
+		String aisleId = bay.getLocationIdToParentLevel(Aisle.class);
+		Assert.assertEquals(aisleId, "A1.B1");
+
+		String facilityId = bay.getLocationIdToParentLevel(Facility.class);
+		Assert.assertEquals(facilityId, "F1.A1.B1");
+	}
+}
