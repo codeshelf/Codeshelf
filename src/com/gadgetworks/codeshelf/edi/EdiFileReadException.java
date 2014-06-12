3891c0cb31386dcd3ae4ef986236dba6379e376d
jonranstrom <jonranstrom@gmail.com>
2014-06-12 15:39:49 -0700

    aisle file reader exception

---
+++ src/com/gadgetworks/codeshelf/edi/EdiFileReadException.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ *  CodeShelf
+ *  Copyright (c) 2014-2014, Codeshelf, All rights reserved
+ *******************************************************************************/
+package com.gadgetworks.codeshelf.edi;
+
+// --------------------------------------------------------------------------
+/**
+ *  @author jon ranstrom
+ */
+public class EdiFileReadException extends RuntimeException {
+	// Should this be an Exception? (checked). The java style police say so, but DaoException is not, and database errors are lumped with user input as
+	// something for checked exceptions.
+	// The point of this exception is to throw from some methods slightly deep in the call stack, and then catch at the bean reader level.
+	// Much easier and less code with an unchecked exception.
+
+	// Because RuntimeException is serializable there needs to be a serial version. Is this made up value ok?
+	// DaoException has private static final long	serialVersionUID	= -8652996336581739434L;
+	private static final long	serialVersionUID	= -8652996336581739435L;
+
+	public EdiFileReadException(final String inMsg) {
+		super(inMsg);
+	}
+}
