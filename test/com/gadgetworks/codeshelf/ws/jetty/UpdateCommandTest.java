44d33c3a2d8d224d8b161a76a1a4bc77dc7ae90e
Bjoern Heckel <bjoern.heckel@codeshelf.com>
2014-08-11 16:14:11 -0700

    added one more update object command test case

--- test/com/gadgetworks/codeshelf/ws/jetty/UpdateCommandTest.java
+++ test/com/gadgetworks/codeshelf/ws/jetty/UpdateCommandTest.java
@@ -26,7 +26,7 @@ import com.gadgetworks.codeshelf.ws.jetty.server.ServerMessageProcessor;
 // "ObjectUpdateRequest":{"className":"Che","persistentId":"66575760-00b8-11e4-ba3a-48d705ccef0f","properties":{"description":"1123"},"messageId":"cid_6"}
 
 @RunWith(MockitoJUnitRunner.class)
-public class UpdateCheTest extends DAOTestABC {
+public class UpdateCommandTest extends DAOTestABC {
 
 	@Test
 	public final void testUpdateCheOK() {
@@ -204,6 +204,59 @@ public class UpdateCheTest extends DAOTestABC {
 		
 		HashMap<String,Object> properties = new HashMap<String,Object>();
 		properties.put("foo", "bar");
+		req.setProperties(properties);
+		
+		ServerMessageProcessor processor = new ServerMessageProcessor(daoProvider);
+		ResponseABC response = processor.handleRequest(session, req);
+		Assert.assertTrue(response instanceof ObjectUpdateResponse);
+		
+		ObjectUpdateResponse updateResponse = (ObjectUpdateResponse) response;
+		Assert.assertEquals(ResponseStatus.Fail, updateResponse.getStatus());
+		
+		Object result = updateResponse.getResults();
+		Assert.assertNull(result);		
+	}
+	
+	
+	@Test
+	public final void testUndefinedCheId() {
+		String description1 = "che description";
+		String description2 = "changed che description";
+		
+		MockDaoProvider daoProvider = new MockDaoProvider();
+		
+		MockSession session = new MockSession();
+		session.setId("test-session");
+	
+		ITypedDao<Organization> orgDao = daoProvider.getDaoInstance(Organization.class);
+		ITypedDao<Facility> facDao = daoProvider.getDaoInstance(Facility.class);
+		ITypedDao<CodeshelfNetwork> netDao = daoProvider.getDaoInstance(CodeshelfNetwork.class);
+		ITypedDao<Che> cheDao = daoProvider.getDaoInstance(Che.class);
+
+		Organization organization = new Organization();
+		organization.setOrganizationId("CTEST1.O1");
+		orgDao.store(organization);
+
+		Facility facility = new Facility(Point.getZeroPoint());
+		facility.setParent(organization);
+		facility.setFacilityId("CTEST1.F1");
+		facDao.store(facility);		
+		
+		CodeshelfNetwork network = new CodeshelfNetwork(facility, "N1", "foo", "bar");
+		netDao.store(network);
+		
+		Che che = new Che();
+		che.setDescription(description1);
+		che.setParent(network);
+		che.setDomainId("C1");
+		cheDao.store(che);
+		
+		ObjectUpdateRequest req = new ObjectUpdateRequest();
+		req.setClassName("Che");
+		req.setPersistentId(null);
+		
+		HashMap<String,Object> properties = new HashMap<String,Object>();
+		properties.put("description", description2);
 		req.setProperties(properties);
 		
 		ServerMessageProcessor processor = new ServerMessageProcessor(daoProvider);
