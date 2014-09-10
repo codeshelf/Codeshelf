e4b5c678610d787fd36b210ba15697c011f971c9
4levity <ivan@4levity.net>
2014-09-10 14:57:29 -0700

    Apply lz4 compression to any websocket message >32K in size.

--- src/com/gadgetworks/codeshelf/ws/jetty/io/JsonDecoder.java
+++ src/com/gadgetworks/codeshelf/ws/jetty/io/JsonDecoder.java
@@ -1,9 +1,14 @@
 package com.gadgetworks.codeshelf.ws.jetty.io;
 
+import java.io.UnsupportedEncodingException;
+
 import javax.websocket.DecodeException;
 import javax.websocket.Decoder;
 import javax.websocket.EndpointConfig;
 
+import net.jpountz.lz4.LZ4Factory;
+
+import org.apache.shiro.codec.Base64;
 import org.atteo.classindex.ClassIndex;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,7 +21,8 @@ import com.gadgetworks.codeshelf.ws.jetty.protocol.message.MessageABC;
 public class JsonDecoder implements Decoder.Text<MessageABC> {
 
 	private static final Logger	LOGGER = LoggerFactory.getLogger(JsonDecoder.class);
-
+	LZ4Factory lz4Factory = LZ4Factory.safeInstance();
+	
 	public JsonDecoder() {
 	}
 
@@ -30,8 +36,25 @@ public class JsonDecoder implements Decoder.Text<MessageABC> {
 
 	@Override
 	public MessageABC decode(String rawMessage) throws DecodeException {
+		//LOGGER.debug("Compressed message: "+rawMessage+ " ("+rawMessage.length()+" bytes)");
+		String decompressedMessage;
+		if(rawMessage.charAt(0)!='{') {
+			// not valid json? compressed/base64
+			
+			byte[] rawBytes=rawMessage.getBytes();
+			byte[] decomBytes = lz4Factory.safeDecompressor().decompress(Base64.decode(rawBytes),JsonEncoder.JSON_COMPRESS_MAXIMUM);
+			try {
+				decompressedMessage = new String(decomBytes,"UTF-8");
+			} catch (UnsupportedEncodingException e1) {
+				LOGGER.error("Not UTF-8 request", e1);
+				throw new DecodeException("", "Not UTF-8 request", e1);
+			}
+		} else{
+			// regular uncompressed json
+			decompressedMessage=rawMessage;
+		}
 		try {
-			LOGGER.debug("Decoding message: "+rawMessage);
+			LOGGER.debug("Decoding message: "+decompressedMessage);
 			ObjectMapper mapper = new ObjectMapper();
 			// register classes
 			Iterable<Class<? extends MessageABC>> requestClasses = ClassIndex.getSubclasses(MessageABC.class);
@@ -41,12 +64,12 @@ public class JsonDecoder implements Decoder.Text<MessageABC> {
 			mapper.registerSubtypes(Point.class);
 			mapper.registerSubtypes(Vertex.class);
 			// decode message
-			MessageABC message = mapper.readValue(rawMessage, MessageABC.class);
+			MessageABC message = mapper.readValue(decompressedMessage, MessageABC.class);
 			return message;
 		}
 		catch (Exception e) {
-			LOGGER.error("Failed to decode request: "+rawMessage, e);
-			throw new DecodeException(rawMessage, "Failed to decode request", e);
+			LOGGER.error("Failed to decode request: "+decompressedMessage, e);
+			throw new DecodeException(decompressedMessage, "Failed to decode request", e);
 		}
 	}
 
--- src/com/gadgetworks/codeshelf/ws/jetty/io/JsonEncoder.java
+++ src/com/gadgetworks/codeshelf/ws/jetty/io/JsonEncoder.java
@@ -4,6 +4,9 @@ import javax.websocket.EncodeException;
 import javax.websocket.Encoder;
 import javax.websocket.EndpointConfig;
 
+import net.jpountz.lz4.LZ4Factory;
+
+import org.apache.ws.commons.util.Base64;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,9 +14,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gadgetworks.codeshelf.ws.jetty.protocol.message.MessageABC;
 
 public class JsonEncoder implements Encoder.Text<MessageABC> {
+	final public static int JSON_COMPRESS_THRESHOLD = 32768;
+	final public static int JSON_COMPRESS_MAXIMUM = 1048576;
 
 	private static final Logger	LOGGER = LoggerFactory.getLogger(JsonEncoder.class);
-
+	LZ4Factory lz4Factory = LZ4Factory.safeInstance();
+	
 	@Override
 	public void init(EndpointConfig ec) {
 	}
@@ -28,8 +34,16 @@ public class JsonEncoder implements Encoder.Text<MessageABC> {
 			String jsonString = null;
 			ObjectMapper mapper = new ObjectMapper();
 			jsonString = mapper.writeValueAsString(message);
+			
 			LOGGER.debug("Encoding message: "+jsonString);
-			return jsonString;
+			
+			if(jsonString.length() >= JSON_COMPRESS_THRESHOLD ) {
+				String compressedJson = Base64.encode(lz4Factory.fastCompressor().compress(jsonString.getBytes("UTF-8")));
+				LOGGER.debug("Compressed "+jsonString.length()+" bytes to "+compressedJson.length()+" bytes");
+				return compressedJson;
+			} else {
+				return jsonString;
+			}
 		} 
 		catch (Exception e) {
 			LOGGER.error("Failed to encode response", e);
