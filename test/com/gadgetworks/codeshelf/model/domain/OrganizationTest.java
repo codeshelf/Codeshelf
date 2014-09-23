de32eb7f19cc3d12788884d76f68223f5743279a
Paul Monteiro <paul.monteiro@codeshelf.com>
2014-09-23 12:42:37 -0700

    Moved workinstruction export to WorkService Made the sending of work instructions to IronMq asynchronous Refactored to unit test the logic of sending and formatting work instructions to the queue
    Conflicts:
    	src/com/gadgetworks/codeshelf/model/domain/IronMqService.java
    	src/com/gadgetworks/codeshelf/model/domain/Organization.java
    	test/com/gadgetworks/codeshelf/model/WorkServiceTest.java

---
+++ test/com/gadgetworks/codeshelf/model/domain/OrganizationTest.java
@@ -0,0 +1,14 @@
+package com.gadgetworks.codeshelf.model.domain;
+
+import org.junit.Test;
+
+public class OrganizationTest extends DomainTestABC {
+
+	
+	@Test
+	public void createFacility() {
+		Organization organization = new Organization("ORG");
+		mOrganizationDao.store(organization);
+		organization.createFacility("FACILITY NAME", "INDESCRIPTIONS", 0.0, 1.0);
+	}
+}
