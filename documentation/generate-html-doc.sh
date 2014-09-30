22652b05251a5ecd0637ae689b689ceb0830b873
Paul Monteiro <paul.monteiro@codeshelf.com>
2014-09-30 14:47:27 -0700

    Updated work instruction export documentation

---
+++ documentation/generate-html-doc.sh
@@ -0,0 +1,5 @@
+# requires ruby
+# gem install asciidoctor
+asciidoctor -a stylesheet=codeshelf-doc.css workinstruction.asciidoc
+#asciidoctor -b docbook workinstruction.asciidoc
+#fopub workinstruction.xml
