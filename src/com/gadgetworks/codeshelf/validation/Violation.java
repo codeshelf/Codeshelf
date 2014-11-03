87da29fa7346e0c819e65ac41cd42ece28410a93
Paul Monteiro <paul.monteiro@codeshelf.com>
2014-11-03 12:39:59 -0800

    Change the field of violation to description

--- src/com/gadgetworks/codeshelf/validation/Violation.java
+++ src/com/gadgetworks/codeshelf/validation/Violation.java
@@ -13,23 +13,23 @@ public class Violation {
 	private Exception failure;
 	
 	@Getter
-	private String failureDescription;
+	private String description;
 	
 	public Violation(Object failedLine, String failureDescription) {
 		super();
 		this.failedLine = failedLine;
-		this.failureDescription = failureDescription;
+		this.description = failureDescription;
 	}
 
 	public Violation(Object inRelatedObject, Exception inException) {
 		this.failedLine = inRelatedObject;
-		this.failureDescription = inException.toString();
+		this.description = inException.toString();
 		this.failure = inException;
 	}
 	
 	public Violation(Object inRelatedObject, Exception inException, String furtherDetail) {
 		this.failedLine = inRelatedObject;
-		this.failureDescription = furtherDetail;
+		this.description = furtherDetail;
 		this.failure = inException;
 	}
 	
