5ef490268682d8ccd5f3c195c4184da31e2d20d5
Ilya Landa <IlyaALanda@gmail.com>
2014-12-17 13:39:36 -0800

    Added Get Facilities List API call

---
+++ src/com/gadgetworks/codeshelf/apiresources/FacilityResource.java
@@ -0,0 +1,41 @@
+package com.gadgetworks.codeshelf.apiresources;
+
+import java.util.List;
+
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.gadgetworks.codeshelf.model.domain.Facility;
+import com.gadgetworks.codeshelf.platform.persistence.PersistenceService;
+
+@Path("/facilities")
+public class FacilityResource {
+	private PersistenceService persistence = PersistenceService.getInstance();
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@OneToMany(fetch=FetchType.EAGER)
+	public Response getAllFacilities() {
+		ErrorResponse errors = new ErrorResponse();
+		try {
+			persistence.beginTenantTransaction();
+			List<Facility> facilities = Facility.DAO.getAll();
+			//Initialize lazy children collections in Facilities 
+			for (Facility f : facilities){
+				f.initialize();
+			}
+			return Response.status(HttpServletResponse.SC_OK).entity(facilities).header("Access-Control-Allow-Origin", "*").build();
+		} catch (Exception e) {
+			errors.processException(e);
+			return errors.buildResponse();
+		} finally {
+			persistence.commitTenantTransaction();
+		}
+	}
+}
