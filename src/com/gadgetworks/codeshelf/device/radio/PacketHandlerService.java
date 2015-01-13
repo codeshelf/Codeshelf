4e9f4afb5a0db9b6bf6dc12ddea2ed932a44e107
Saba Khalilnaji <saba.khalilnaji@codeshelf.com>
2015-01-13 11:15:56 -0800

    fixed packet service

--- src/com/gadgetworks/codeshelf/device/radio/PacketHandlerService.java
+++ src/com/gadgetworks/codeshelf/device/radio/PacketHandlerService.java
@@ -3,30 +3,33 @@ package com.gadgetworks.codeshelf.device.radio;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ConcurrentMap;
-import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import com.gadgetworks.flyweight.command.IPacket;
 import com.gadgetworks.flyweight.command.NetAddress;
 import com.google.common.collect.Maps;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 
 /**
  * The packet handler service is thread-safe and allows multiple threads to call handle. The handle method will add the packet to a queue
- * to be processed. If that queue is full, the handle method will block and wait until there is room in the queue. Each submitted packed is processed in order by
+ * to be processed. If that queue is full, the handle method will throw an exception. Each submitted packed is processed in order by
  * the SourceAddr and in parallel across different SourceAddres. The parallelism of this service defaults to the number of available cores.
  * 
  * @author saba
  *
  */
 public class PacketHandlerService {
-	private final Executor		executor	= Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+	private final ExecutorService									executor				= Executors.newFixedThreadPool(Runtime.getRuntime()
+																								.availableProcessors(),
+																								new ThreadFactoryBuilder().setNameFormat("pckt-hndlr-%s")
+																									.build());
 	private final ConcurrentMap<NetAddress, BlockingQueue<IPacket>>	queueMap				= Maps.newConcurrentMap();
 	private static final int							MAX_PACKET_QUEUE_SIZE	= 50;
 
-	public void handle(IPacket packet) throws InterruptedException {
+	public boolean handle(IPacket packet) {
 
-		//Add packet to queue in thread-safe manner. Multiple threads can call handle at the same time
-		//Right now, we only have 1 reader thread that calls handle
+		//Get queue from queueMap in thread-safe manner. Multiple threads can call handle at the same time
 		BlockingQueue<IPacket> queue = queueMap.get(packet.getSrcAddr());
 		if (queue == null) {
 			queue = new ArrayBlockingQueue<IPacket>(MAX_PACKET_QUEUE_SIZE);
@@ -36,24 +39,28 @@ public class PacketHandlerService {
 			}
 		}
 
-		//We synchronize on the queue (even though its thread-safe) because we must perform 2 actions atomically
-		boolean shouldSubmitHandler = false;
+		//We synchronize on the queue before modifying it (even though its thread-safe) because we must perform 3 actions atomically
+		boolean wasAddedToQueue = false;
 		synchronized (queue) {
-			//If its empty, then we can resubmit a handler to the executor since no other handler should be running
-			//and we're about to add a packet to be handled
-			shouldSubmitHandler = queue.isEmpty();
 
-			//PUT will block until space is available DEADLOCK HERE
-			queue.put(packet);
+			boolean wasQueueOriginallyEmpty = queue.isEmpty();
 
+			//OFFER will not block and return boolean for success. If we block here, we will have a deadlock
+			wasAddedToQueue = queue.offer(packet);
+
+			//If it was originally empty, then we can resubmit a handler to the executor since no other handler should be running.
+			//But we should submit only if we succeeded in adding the packet to the queue!
+			if (wasAddedToQueue && wasQueueOriginallyEmpty) {
+				executor.submit(new PacketHandler(queue));
+			}
 		}
 
-		if (shouldSubmitHandler) {
-			executor.execute(new PacketHandler(queue));
-		}
+		return wasAddedToQueue;
 
+	}
 
-
+	public void shutdown() {
+		executor.shutdown();
 	}
 
 	private final class PacketHandler implements Runnable {
@@ -75,16 +82,18 @@ public class PacketHandlerService {
 				//Handle packet
 			} catch (Exception e) {
 				//Log Error
-			}
+			} finally {
+				//The finally block ensures that we will always pop this packet off the queue, no matter if we have some uncaught exception or not (like a generic throwable)
 
-			//We synchronize on the queue (even though its thread-safe) because we must perform 2 actions atomically
-			synchronized (queue) {
-				//Remove packet we JUST finished processing
-				queue.poll();
+				//We synchronize on the queue (even though its thread-safe) because we must perform 2 actions atomically
+				synchronized (queue) {
+					//Remove packet we JUST finished processing
+					queue.poll();
 
-				if (!queue.isEmpty()) {
-					//Resubmit this runnable if its not empty yet and let the other submitted PacketHandlers get a fair chance
-					executor.execute(this);
+					if (!queue.isEmpty()) {
+						//Resubmit this runnable if its not empty yet and let the other submitted PacketHandlers get a fair chance
+						executor.submit(this);
+					}
 				}
 			}
 
