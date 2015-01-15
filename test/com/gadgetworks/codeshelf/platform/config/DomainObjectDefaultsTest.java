7825e46c0f793889cac8b17eb0812547bb4a954d
Bjoern Heckel <bjoern.heckel@codeshelf.com>
2015-01-15 11:31:15 -0800

    added two property defaults

---
+++ test/com/gadgetworks/codeshelf/platform/config/DomainObjectDefaultsTest.java
@@ -0,0 +1,41 @@
+package com.gadgetworks.codeshelf.platform.config;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.gadgetworks.codeshelf.model.dao.PropertyDao;
+import com.gadgetworks.codeshelf.model.domain.DomainObjectProperty;
+import com.gadgetworks.codeshelf.model.domain.DomainObjectPropertyDefault;
+import com.gadgetworks.codeshelf.model.domain.DomainTestABC;
+import com.gadgetworks.codeshelf.model.domain.Facility;
+
+public class DomainObjectDefaultsTest extends DomainTestABC {
+	
+	@SuppressWarnings("unused")
+	private static final Logger	LOGGER	= LoggerFactory.getLogger(DomainObjectDefaultsTest.class);
+
+	@Test
+	public void ensureRequiredDefaultsExist() {
+		PropertyDao cfgServ = PropertyDao.getInstance();
+
+		beginTenantTransaction();
+		
+		// check LOCAPICK default
+		DomainObjectPropertyDefault value = cfgServ.getPropertyDefault(Facility.class.getSimpleName(), DomainObjectProperty.LOCAPICK);
+		assertNotNull(value);
+		assertEquals(false,value.getBooleanValue());
+		
+		// check EACHMULT default
+		value = cfgServ.getPropertyDefault(Facility.class.getSimpleName(), DomainObjectProperty.EACHMULT);
+		assertNotNull(value);
+		assertEquals(false,value.getBooleanValue());
+		
+		commitTenantTransaction();
+	}
+	
+}
