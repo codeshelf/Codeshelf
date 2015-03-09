e3b8fa89514909f1d96fc4c1906859f458ebcae8
Bjoern Heckel <bjoern.heckel@codeshelf.com>
2015-03-09 16:00:13 -0700

    applied order import optimization to v13

---
+++ src/main/java/com/codeshelf/platform/caching/DomainObjectCache.java
@@ -0,0 +1,94 @@
+package com.codeshelf.platform.caching;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Property;
+import org.hibernate.criterion.Restrictions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.codeshelf.model.dao.ITypedDao;
+import com.codeshelf.model.domain.DomainObjectABC;
+import com.codeshelf.model.domain.IDomainObject;
+import com.codeshelf.model.domain.ItemMaster;
+
+public class DomainObjectCache<T extends DomainObjectABC> {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(DomainObjectCache.class);
+	
+	@Getter @Setter
+	int maxPrefetchSize = 10000;
+
+	Map<String,T> objectCache =  new HashMap<String,T>();
+	
+	final ITypedDao<T> dao;
+	
+	IDomainObject parent = null;
+	
+	public DomainObjectCache(ITypedDao<T> dao) {		
+		this.dao = dao;
+	}
+		
+	public void loadAll() {
+		Criteria crit = this.dao.createCriteria();
+		if (this.dao.getDaoClass().equals(ItemMaster.class)) {
+			crit.addOrder(Property.forName("updated").desc());
+		}
+		crit.setMaxResults(maxPrefetchSize);
+		List<T> list = dao.findByCriteriaQuery(crit);
+		for (T item : list) {
+			objectCache.put(item.getDomainId(), item);
+		}
+	}
+	
+	public void load(IDomainObject parent) {
+		this.parent = parent;
+		UUID persistentId = parent.getPersistentId();
+		Criteria crit = this.dao.createCriteria();
+		crit.add(Restrictions.eq("parent.persistentId", persistentId));
+		// little bit of a hack: sort by update time stamp for item
+		// master to implement LRU prefetching.  would be better to have 
+		// update time stamp on all domain objects.
+		if (this.dao.getDaoClass().equals(ItemMaster.class)) {
+			crit.addOrder(Property.forName("updated").desc());
+		}
+		crit.setMaxResults(maxPrefetchSize);
+		List<T> list = dao.findByCriteriaQuery(crit);
+		for (T item : list) {
+			objectCache.put(item.getDomainId(), item);
+		}
+	}
+	
+	public T get(String domainId) {
+		T obj = this.objectCache.get(domainId); 
+		if (obj==null) {
+			// try to retrieve from database on miss
+			obj = this.dao.findByDomainId(this.parent, domainId);
+			if (obj!=null) {
+				// add to cache
+				objectCache.put(obj.getDomainId(), obj);
+			}
+		}
+		return obj;
+	}
+
+	public void put(T obj) {
+		objectCache.put(obj.getDomainId(), obj);		
+	}
+
+	public int size() {
+		return this.objectCache.size();
+	}
+	
+	public void reset() {
+		this.objectCache.clear();
+	}
+
+}
---
+++ src/test/java/com/codeshelf/platform/caching/DomainObjectCacheTest.java
@@ -0,0 +1,57 @@
+package com.codeshelf.platform.caching;
+
+import java.sql.Timestamp;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.codeshelf.model.domain.DomainTestABC;
+import com.codeshelf.model.domain.Facility;
+import com.codeshelf.model.domain.ItemMaster;
+import com.codeshelf.model.domain.UomMaster;
+
+public class DomainObjectCacheTest extends DomainTestABC {
+
+	@Test
+	public void testReturnActiveLocationsOnly() {
+		this.getTenantPersistenceService().beginTransaction();
+		Facility facility = this.createFacility();
+		
+		DomainObjectCache<ItemMaster> cache = new DomainObjectCache<ItemMaster>(ItemMaster.DAO);
+		cache.loadAll();
+		Assert.assertEquals(0, cache.size());
+		
+		UomMaster uomMaster = new UomMaster();
+		uomMaster.setUomMasterId("EA");
+		uomMaster.setParent(facility);
+		UomMaster.DAO.store(uomMaster);
+		facility.addUomMaster(uomMaster);
+		ItemMaster im = new ItemMaster();
+		im.setParent(facility);
+		im.setDomainId("IM#1");
+		im.setItemId("IM#1");
+		im.setStandardUom(uomMaster);
+		im.setUpdated(new Timestamp(System.currentTimeMillis()));
+		ItemMaster.DAO.store(im);
+		this.getTenantPersistenceService().commitTransaction();
+
+		this.getTenantPersistenceService().beginTransaction();
+		cache.loadAll();
+		Assert.assertEquals(1, cache.size());
+		ItemMaster im2 = new ItemMaster();
+		im2.setParent(facility);
+		im2.setDomainId("IM#2");
+		im2.setItemId("IM#2");
+		im2.setStandardUom(uomMaster);
+		im2.setUpdated(new Timestamp(System.currentTimeMillis()));
+		ItemMaster.DAO.store(im2);
+		this.getTenantPersistenceService().commitTransaction();
+
+		this.getTenantPersistenceService().beginTransaction();
+		cache.get("IM#MISS");
+		Assert.assertEquals(1, cache.size());
+		cache.get("IM#2");
+		Assert.assertEquals(2, cache.size());
+		this.getTenantPersistenceService().commitTransaction();
+	}
+}
