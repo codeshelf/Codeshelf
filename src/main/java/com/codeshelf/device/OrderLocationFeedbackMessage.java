09b3d8e40a389fc2369af688da688586f4c051b8
jonranstrom <jonranstrom@gmail.com>
2015-04-03 11:50:53 -0700

    DEV-728 order feedback message

--- src/main/java/com/codeshelf/device/OrderLocationFeedbackMessage.java
+++ src/main/java/com/codeshelf/device/OrderLocationFeedbackMessage.java
@@ -2,11 +2,15 @@ package com.codeshelf.device;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
 
 import com.codeshelf.model.OrderStatusEnum;
+import com.codeshelf.model.domain.LedController;
 import com.codeshelf.model.domain.Location;
 import com.codeshelf.model.domain.OrderHeader;
 import com.codeshelf.model.domain.OrderLocation;
@@ -16,55 +20,85 @@ import com.google.gson.annotations.SerializedName;
 
 public class OrderLocationFeedbackMessage extends MessageABC {
 
-	@Accessors(prefix = "m")
-	@Getter
-	@Setter
-	@Expose
-	private String	mLocationName;
+	private static final Logger	LOGGER	= LoggerFactory.getLogger(OrderLocationFeedbackMessage.class);
 
 	@Accessors(prefix = "m")
 	@Getter
 	@Setter
 	@Expose
-	private String	mOrderId;				// not initially used. Just seems like site controller could use it.
+	private String				mControllerId;
 
 	@Accessors(prefix = "m")
 	@Getter
 	@Setter
 	@Expose
-	private Boolean	mHasAnyOrderAtAll;
+	private Byte				mPosition;
+	// as a Byte, similar to PosControllerInstr. Someday might change to an Integer
 
 	@Accessors(prefix = "m")
 	@Getter
 	@Setter
 	@Expose
-	private Boolean	mOrderFullyComplete;
+	private String				mLocationName;
 
 	@Accessors(prefix = "m")
 	@Getter
 	@Setter
 	@Expose
-	private Boolean	mOrderCompleteThisArea; // not initially used. 
+	private String				mOrderId;																// not initially used. Just seems like site controller could use it.
+
+	@Accessors(prefix = "m")
+	@Getter
+	@Setter
+	@Expose
+	private Boolean				mHasAnyOrderAtAll;
+
+	@Accessors(prefix = "m")
+	@Getter
+	@Setter
+	@Expose
+	private Boolean				mOrderFullyComplete;
+
+	@Accessors(prefix = "m")
+	@Getter
+	@Setter
+	@Expose
+	private Boolean				mOrderCompleteThisArea;													// not initially used. 
 
 	public OrderLocationFeedbackMessage(OrderLocation ol) {
+		Location loc = ol.getLocation();
+		setLocationDependentFields(loc);
 		setHasAnyOrderAtAll(true);
 		OrderHeader oh = ol.getParent();
 		setOrderId(oh.getDomainId());
-		setLocationName(ol.getDomainId());
 		setOrderFullyComplete(OrderStatusEnum.COMPLETE.equals(oh.getStatus()));
 		setOrderCompleteThisArea(getOrderFullyComplete()); // fix later
 	}
 
 	public OrderLocationFeedbackMessage(Location loc) {
 		// Use this constructor to intentionally tell site controller there is nothing for this slot.
+		setLocationDependentFields(loc);
 		setHasAnyOrderAtAll(false);
+		setOrderId("");
+		setOrderFullyComplete(false);
+		setOrderCompleteThisArea(false);
+	}
+
+	private void setLocationDependentFields(Location loc) {
+		// Device GUID and position
+		LedController controller = loc.getEffectiveLedController();
+		if (controller == null)
+			LOGGER.error("OrderLocationFeedbackMessage constructor called for location without controller");
+		else
+			setControllerId(controller.getDeviceGuidStr());
+		Integer index = loc.getPosconIndex();
+		setPosition(index.byteValue());
+
+		// Location name in a uniform way, so the map in PosManagerDeviceLogic can set and clear.
 		String locName = loc.getPrimaryAliasId();
 		if (locName.isEmpty())
 			locName = loc.getNominalLocationIdExcludeBracket();
-		setOrderId("");		
 		setLocationName(locName);
-		setOrderFullyComplete(false);
-		setOrderCompleteThisArea(false);
 	}
 
 }
