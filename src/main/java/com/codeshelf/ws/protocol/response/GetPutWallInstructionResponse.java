66bac1ca8c5093aa6c299c6894c638c42e491c4e
4levity <ivan.cooper@gmail.com>
2015-04-13 14:03:52 -0700

    fix intermittent crossbatch test failures

--- src/main/java/com/codeshelf/ws/protocol/response/GetPutWallInstructionResponse.java
+++ src/main/java/com/codeshelf/ws/protocol/response/GetPutWallInstructionResponse.java
@@ -6,9 +6,6 @@ import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.codeshelf.model.domain.WorkInstruction;
 import com.codeshelf.persistence.TenantPersistenceService;
 
