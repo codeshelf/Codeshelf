cfdd37a4111fc7a9910b11911b1cf9bc4b96bfc0
Ilya Landa <IlyaALanda@gmail.com>
2015-05-05 11:12:52 -0700

    Worked on Groovy pick script

--- src/main/groovy/com/codeshelf/sim/worker/PickOrders.groovy
+++ src/main/groovy/com/codeshelf/sim/worker/PickOrders.groovy
@@ -6,43 +6,53 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
-class Picker {
-	int WAIT_TIME = 10000
+class PickOrders {
+	int WAIT_TIMEOUT = 10000
 	
-	def pickOrders(PickSimulator picker, String container, double chanceSkipUpc, double chanceShort) {
-		println "picking order in container " + container + " " + picker
+	def pickOrders(PickSimulator picker, ArrayList<String> containerList, double chanceSkipUpc, double chanceShort) {
+		if (containerList == null || containerList.isEmpty()) {
+			return "Enpty container list";
+		}
+				
+		println "picking order in containers " + containerList; 
 		//Reset CHE to IDLE
 		picker.logout()
-		picker.waitForCheState(CheStateEnum.IDLE, WAIT_TIME)
+		picker.waitForCheState(CheStateEnum.IDLE, WAIT_TIMEOUT)
 	
 		//Login into CHE
 		picker.loginAndSetup("GroovyWorker1")
-		picker.waitForCheState(CheStateEnum.CONTAINER_SELECT, WAIT_TIME)
+		picker.waitForCheState(CheStateEnum.CONTAINER_SELECT, WAIT_TIMEOUT)
 		//Set up order on the CHE and try to start pick
-		picker.setupContainer(container, "1");
+		//StringBuilder containersStr = new StringBuilder();
+		int position = 1;
+		for (container in containerList) {
+			//containersStr.append(container).append(" ");
+			picker.setupContainer(container, position++ + "");
+		}
+		//picker.setupContainer(container, "1");
 		picker.scanCommand(CheDeviceLogic.STARTWORK_COMMAND);
 		boolean usesSummaryState = picker.getCheDeviceLogic().usesSummaryState();
 		if (usesSummaryState) {
-			picker.waitForOneOfCheStates([CheStateEnum.SETUP_SUMMARY, CheStateEnum.NO_WORK], WAIT_TIME);
+			picker.waitForOneOfCheStates([CheStateEnum.SETUP_SUMMARY, CheStateEnum.NO_WORK], WAIT_TIMEOUT);
 		} else {
-			picker.waitForOneOfCheStates([CheStateEnum.LOCATION_SELECT, CheStateEnum.NO_WORK], WAIT_TIME);
+			picker.waitForOneOfCheStates([CheStateEnum.LOCATION_SELECT, CheStateEnum.NO_WORK], WAIT_TIMEOUT);
 		}
 		CheStateEnum state = picker.getCurrentCheState();
-		//If CHE is in NO_WORK or NO_WORK_CURR_PATH state, exit without crashing.
+		String noWorkMsg = "No work for these containers: " + containerList;
 		if (state == CheStateEnum.NO_WORK){
-			println("No work for this order. Check if order in container " + container + " exists");
 			picker.logout();
-			return "No work found (during preview) for container " + container;
+			println(noWorkMsg);
+			return noWorkMsg;
 		}
 	
 		picker.scanCommand(CheDeviceLogic.STARTWORK_COMMAND);
-		picker.waitForOneOfCheStates([CheStateEnum.SCAN_SOMETHING, CheStateEnum.NO_WORK,  CheStateEnum.SETUP_SUMMARY], WAIT_TIME);
+		picker.waitForOneOfCheStates([CheStateEnum.SCAN_SOMETHING, CheStateEnum.NO_WORK,  CheStateEnum.SETUP_SUMMARY], WAIT_TIMEOUT);
 		state = picker.getCurrentCheState();
 		//If Che immediately arrives at the end-of-work state, stop processing this order
 		if (state == CheStateEnum.NO_WORK || (usesSummaryState && state == CheStateEnum.SETUP_SUMMARY)){
-			println("No work for this order. Check if order in container " + container + " exists");
 			picker.logout();
-			return "No work found (during start pick) for container " + container;
+			println(noWorkMsg);
+			return noWorkMsg;
 		}
 	
 		def List<WorkInstruction> picksList = picker.getAllPicksList();
@@ -56,27 +66,33 @@ class Picker {
 				break;
 			} else if (instruction.isHousekeeping()) {
 				//Skip Housekeeping instruction
-				picker.waitForCheState(CheStateEnum.DO_PICK, WAIT_TIME);
+				picker.waitForCheState(CheStateEnum.DO_PICK, WAIT_TIMEOUT);
 				picker.pickItemAuto();
 			} else {
 				//Process normal instruction
-				picker.waitForCheState(CheStateEnum.SCAN_SOMETHING, WAIT_TIME);
-				//Scan UPC or skip it
-				if (chance(chanceSkipUpc)) {
-					println("Skip UPC scan");
-					picker.scanSomething(CheDeviceLogic.SKIP_SCAN);
-				} else {
-					println("Scan UPC");
-					picker.scanSomething(instruction.getItemId());
+				picker.waitForOneOfCheStates([CheStateEnum.SCAN_SOMETHING, CheStateEnum.DO_PICK], WAIT_TIMEOUT);
+				state = picker.getCurrentCheState();
+				//When picking multiple orders containing same items, UPC scan is only needed once per item
+				if (state == CheStateEnum.SCAN_SOMETHING) {
+					//Scan UPC or skip it
+					if (chance(chanceSkipUpc)) {
+						println("Skip UPC scan");
+						picker.scanSomething(CheDeviceLogic.SKIP_SCAN);
+					} else {
+						println("Scan UPC");
+						picker.scanSomething(instruction.getItemId());
+					}
+					picker.waitForCheState(CheStateEnum.DO_PICK, WAIT_TIMEOUT);
 				}
-				picker.waitForCheState(CheStateEnum.DO_PICK, WAIT_TIME);
+				//Thread.sleep(1000);
 				//Pick item or short it
 				if (chance(chanceShort)) {
 					println("Short Item");
 					picker.scanCommand("SHORT");
-					picker.waitForCheState(CheStateEnum.SHORT_PICK, WAIT_TIME);
-					picker.pick(1,0);
-					picker.waitForCheState(CheStateEnum.SHORT_PICK_CONFIRM, WAIT_TIME);
+					picker.waitForCheState(CheStateEnum.SHORT_PICK, WAIT_TIMEOUT);
+					int button = picker.buttonFor(picker.getActivePick());
+					picker.pick(button,0);
+					picker.waitForCheState(CheStateEnum.SHORT_PICK_CONFIRM, WAIT_TIMEOUT);
 					picker.scanCommand("YES");
 				} else {
 					println("Pick Item");
@@ -87,20 +103,21 @@ class Picker {
 		
 		//Finish test by logging out
 		picker.logout();
-		picker.waitForCheState(CheStateEnum.IDLE, WAIT_TIME);
-		return "Finished picking container " + container;
+		picker.waitForCheState(CheStateEnum.IDLE, WAIT_TIMEOUT);
+		return "Finished picking containers " + containerList;
 	}
 	
 	boolean chance(double percentage) {
 		double rnd = Math.random();
 		return rnd < percentage;
 	}
-	
-	def importAndPickOrders(PickSimulator picker, String filename, String outFile) {
+		
+	def importAndPickOrders(PickSimulator picker, String facilityId, String filename, int batchSize, double chanceSkipUpc, double chanceShort) {
+		String outFile = 'importedOrders.txt';
 		def sout = new StringBuffer(), serr = new StringBuffer()
-		def proc = ['src/test/resources/scripts/importOrders.sh', 'simulate@example.com', 'testme', '7d16e862-32db-4270-8eda-aff14629aceb', filename, outFile].execute();
+		def proc = ['src/test/resources/scripts/importOrders.sh', 'simulate@example.com', 'testme', facilityId, filename, outFile].execute();
 		proc.consumeProcessOutput(sout, serr)
-		//Can kill site controller
+		//Warning: Can kill site controller for import isn't done by then
 		proc.waitForOrKill(1000 * 60 * 25);
 		if (sout.length() > 0) {
 			println("out> $sout");
@@ -128,16 +145,15 @@ class Picker {
 			}
 		}
 		
-		//PickOrders pickClass = new PickOrders();
-		//pickClass.pickOrders(picker, "26768711", 0, 0);
+		ArrayList containerList = new ArrayList<String>();
 		for (String container : containers) {
-			pickOrders(picker, container, (double)0.0, (double)0.0);
-		}	
+			containerList.add(container);
+			if (containerList.size >= batchSize) {
+				pickOrders(picker, containerList, chanceSkipUpc, chanceShort);
+				containerList = new ArrayList<String>();
+			}
+		}
+		pickOrders(picker, containerList, chanceSkipUpc, chanceShort);
 		return;
 	}
 }
-
-picker = new PickSimulator(deviceManager, "0x0000009B");
-filename = '../../Desktop/Orders/orders_10.csv';
-outFile = 'importedOrders.txt'
-new Picker().importAndPickOrders(picker, filename, 'importedOrders.txt')
