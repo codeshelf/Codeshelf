cf3a7dd8ec723281dbfd755b752cfe15db414ee7
4levity <ivan@4levity.net>
2015-07-30 16:34:38 -0700

    app server sitecon status health check fails if there are sitecons failing to get upgraded

--- src/main/java/com/codeshelf/metrics/ActiveSiteControllerHealthCheck.java
+++ src/main/java/com/codeshelf/metrics/ActiveSiteControllerHealthCheck.java
@@ -1,13 +1,26 @@
 package com.codeshelf.metrics;
 
 import java.util.Collection;
+import java.util.List;
 
+import com.codeshelf.manager.User;
+import com.codeshelf.manager.service.TenantManagerService;
 import com.codeshelf.ws.server.WebSocketConnection;
 import com.codeshelf.ws.server.WebSocketConnection.State;
 import com.codeshelf.ws.server.WebSocketManagerService;
 
+/**
+ * health check fails if there are NO site controllers connected
+ * OR (new) if there is any site controller, connected or not, that seems to be failing upgrade attempts. 
+ * (i.e. it needs upgrade + has multiple login attempts failing due to bad version) 
+ * 
+ * @author ivan
+ *
+ */
 public class ActiveSiteControllerHealthCheck extends CodeshelfHealthCheck {
 
+	private static final int	MAX_BAD_VERSION_LOGIN_TRIES	= 40; // about 20 minutes of trying to log in
+	
 	WebSocketManagerService sessionManager;
 	
 	public ActiveSiteControllerHealthCheck(WebSocketManagerService sessionManager) {
@@ -24,12 +37,22 @@ public class ActiveSiteControllerHealthCheck extends CodeshelfHealthCheck {
     			c++;
     		}
     	}
-    	if (c>1) {
-            return Result.healthy(c+" active site controllers connected");
+    	if (c==0) {
+        	return Result.unhealthy("No site controllers connected");
+    	} // else check for failing upgrades
+
+    	int badVersions = 0;
+    	List<User> sitecons = TenantManagerService.getInstance().getSiteControllerUsers(true);
+    	for(User sitecon : sitecons) {
+    		if(sitecon.getBadVersionLoginTries() > MAX_BAD_VERSION_LOGIN_TRIES) {
+    			badVersions++;
+    		}
     	}
-    	else if (c==1) {
-            return Result.healthy(c+" active site controller connected");    		
-    	}
-    	return Result.unhealthy("No site controller connected");
+    	if(badVersions > 0) {
+    		return Result.unhealthy(badVersions+" site controllers have not been upgraded");
+    	} // else
+    	
+    	return Result.healthy(c+" site controllers connected");
+    	
     }
 }
