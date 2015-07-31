5327f854372557b9e30c066aa641310bd1b79cd1
Paul Monteiro <paul.monteiro@codeshelf.com>
2015-07-31 12:03:43 -0700

    Updated the bean transform defaults based on confluence doc improvements

--- facility-setup-script/extensionBean.groovy
+++ facility-setup-script/extensionBean.groovy
@@ -1,13 +1,14 @@
 /**
- * This is a sample Bean extension point
  * Transforms the values of a PARSED line in an order file.
  * The fields available on the OrderLine object match the documented fields for
  * an order import file
  */
 def OrderImportBeanTransformation(orderLine) {
-    customerNeedsScan = ['SPECIALCUSTOMER', 'SPECIALCUSTOMER2']
-    if (customerNeedsScan.contains(orderLine.customerId)) {
+    shipperIdNoScan = ['UPS', 'ANOTHERSHIPPER']
+    if (shipperIdNoScan.contains(orderLine.shipperId)) {
+        orderLine.needsScan = false;
+    } else {
         orderLine.needsScan = true;
-    }
+   }
     return orderLine;
 }
