678e1357952645040efbccd4af10f83e2bbcd27f
Ilya Landa <IlyaALanda@gmail.com>
2015-09-03 15:19:02 -0700

    Fixed Groovy Extension test

--- src/test/java/com/codeshelf/edi/FacilityAccumulatingExporterTest.java
+++ src/test/java/com/codeshelf/edi/FacilityAccumulatingExporterTest.java
@@ -89,7 +89,7 @@ public class FacilityAccumulatingExporterTest extends HibernateTest {
 				+ "+ 'ORDERSTATUS'.padRight(20) +'^'" //
 				+ "+ '0'.padLeft(10,'0') +'^'" //
 				+ "+ bean.orderId.padRight(20) +'^'" //
-				+ "+ cheId.padRight(7) +'^'" //
+				+ "+ bean.cheId.padRight(7) +'^'" //
 				+ "+ bean.customerId.padRight(2) +'^'" //
 				+ "+ 'OPEN'.padRight(15);" //
 				+ " return returnStr;}";
