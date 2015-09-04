a3e70d074be7bf0744c2f5e27756026472af4725
Paul Monteiro <paul.monteiro@codeshelf.com>
2015-09-04 10:41:07 -0700

    DEV-1090 EvictingQueue so that the exportmessage queue is bounded but favors newer messages Break from poll periodically to check loop conditions

--- src/main/java/com/codeshelf/util/EvictingBlockingQueue.java
+++ src/main/java/com/codeshelf/util/EvictingBlockingQueue.java
@@ -3,19 +3,45 @@ package com.codeshelf.util;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
+import com.google.common.base.Preconditions;
 import com.google.common.util.concurrent.ForwardingBlockingQueue;
 
 //TODO implement evicting
-public class EvictingBlockingQueue<T> extends ForwardingBlockingQueue<T> {
+public class EvictingBlockingQueue<E> extends ForwardingBlockingQueue<E> {
 
-	private final BlockingQueue<T> delegate;
+	private final BlockingQueue<E> delegate;
 	
 	public EvictingBlockingQueue(int capacity) {
-		this.delegate = new ArrayBlockingQueue<T>(capacity);
+		this.delegate = new ArrayBlockingQueue<E>(capacity);
 	}
 	
 	@Override
-	protected BlockingQueue<T> delegate() {
+	protected BlockingQueue<E> delegate() {
 		return delegate;
 	}
+	
+	  /**
+	   * Adds the given element to this queue. If the queue is currently full, the element at the head
+	   * of the queue is evicted to make room.
+	   *
+	   * @return {@code true} always
+	   */
+	  @Override public boolean offer(E e) {
+	    return add(e);
+	  }
+
+	  /**
+	   * Adds the given element to this queue. If the queue is currently full, the element at the head
+	   * of the queue is evicted to make room.
+	   *
+	   * @return {@code true} always
+	   */
+	  @Override public boolean add(E e) {
+	    Preconditions.checkNotNull(e);  // check before removing
+	    if (delegate().remainingCapacity() == 0) {
+		      delegate.remove();
+	    }
+	    delegate.add(e);
+	    return true;
+	  }
 }
