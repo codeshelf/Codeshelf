3224d029b067205cd8ee2611f7f9ccc230ed95f3
Ilya Landa <IlyaALanda@gmail.com>
2015-09-15 14:18:47 -0700

    Fixed and returned a previously broker test

--- src/test/java/com/codeshelf/edi/EdiExporterProviderTest.java
+++ src/test/java/com/codeshelf/edi/EdiExporterProviderTest.java
@@ -8,29 +8,25 @@ import java.util.concurrent.TimeoutException;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import com.codeshelf.model.domain.Facility;
-import com.codeshelf.model.domain.SftpWIsEdiService;
 import com.codeshelf.testframework.HibernateTest;
 
 public class EdiExporterProviderTest extends HibernateTest {
 	
-	//@Test
+	@Test
 	public void testGetsEdiExporterWhenFacilityHasEdiService() throws Exception {
 		EdiExporterProvider subject = startEdiExporter();
 		beginTransaction();
 
 		try {
-			//Facility facility = mock(Facility.class);
 			Facility facility = getFacility();
-			//facility.getedi
-			//SftpWIsEdiService ediService = new SftpWIsEdiService("SFTPWIS");
-			//ediService.setParent(facility);
-			//SftpWIsEdiService.staticGetDao().store(ediService);
-			//when(facility.getEdiExportTransport()).thenReturn(mock(EdiExportTransport.class));
+			Facility mockedFacility = Mockito.spy(facility);
+			when(mockedFacility.getEdiExportTransport()).thenReturn(mock(EdiExportTransport.class));
 			
-			Assert.assertNotNull(subject.getEdiExporter(facility));
-			Assert.assertTrue(subject.getEdiExporter(facility).isRunning());
+			Assert.assertNotNull(subject.getEdiExporter(mockedFacility));
+			Assert.assertTrue(subject.getEdiExporter(mockedFacility).isRunning());
 			
 		}
 		finally{
