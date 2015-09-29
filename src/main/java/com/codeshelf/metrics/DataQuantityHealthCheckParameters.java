acd7aa64e6ac5572e55a271025c8ae2865ee9af7
jonranstrom <jonranstrom@gmail.com>
2015-09-28 17:50:17 -0700

    DEV-1165

--- src/main/java/com/codeshelf/metrics/DataQuantityHealthCheckParameters.java
+++ src/main/java/com/codeshelf/metrics/DataQuantityHealthCheckParameters.java
@@ -16,51 +16,76 @@ public class DataQuantityHealthCheckParameters extends ParameterSetBeanABC {
 	final static int			MAX_WORKINSTRUCTION	= 50000;
 	final static int			MAX_ORDER			= 40000;
 	final static int			MAX_CONTAINERUSE	= 40000;
-	
-	@Getter
-	@Setter
-	protected String	maxOrderDetail;
-	@Getter
-	@Setter
-	protected String	maxWorkInstruction;
-	@Getter
-	@Setter
-	protected String	maxOrder;
-	@Getter
-	@Setter
-	protected String	maxContainerUse;
 
-	
-	@SuppressWarnings("unused")
-	private static final Logger	LOGGER	= LoggerFactory.getLogger(DataQuantityHealthCheckParameters.class);
-	
+	@Getter
+	@Setter
+	protected String			maxOrderDetail;
+	@Getter
+	@Setter
+	protected String			maxWorkInstruction;
+	@Getter
+	@Setter
+	protected String			maxOrder;
+	@Getter
+	@Setter
+	protected String			maxContainerUse;
+
+	private static final Logger	LOGGER				= LoggerFactory.getLogger(DataQuantityHealthCheckParameters.class);
 
 	public DataQuantityHealthCheckParameters() {
 		super();
 		maxOrderDetail = Integer.toString(MAX_ORDERDETAIL);
 		maxWorkInstruction = Integer.toString(MAX_WORKINSTRUCTION);
 		maxOrder = Integer.toString(MAX_ORDER);
-		maxContainerUse = Integer.toString(MAX_CONTAINERUSE);		
-	}
-	
-	public int getMaxOrderDetailValue(){
-		return Integer.valueOf(this.getMaxOrderDetail());
+		maxContainerUse = Integer.toString(MAX_CONTAINERUSE);
 	}
 
-	public int getMaxWorkInstructionValue(){
-		return Integer.valueOf(this.getMaxWorkInstruction());
+	public int getMaxOrderDetailValue() {
+		try {
+			return Integer.valueOf(this.getMaxOrderDetail());
+		} catch (NumberFormatException e) {
+			LOGGER.warn("bad value for maxOrderDetail: {}", getMaxOrderDetail());
+			return MAX_ORDERDETAIL;
+		}
 	}
-	public int getMaxOrderValue(){
-		return Integer.valueOf(this.getMaxOrder());
+
+	public int getMaxWorkInstructionValue() {
+		try {
+			return Integer.valueOf(this.getMaxWorkInstruction());
+		} catch (NumberFormatException e) {
+			LOGGER.warn("bad value for maxWorkInstructionl: {}", getMaxWorkInstruction());
+			return MAX_WORKINSTRUCTION;
+		}
 	}
-	public int getMaxContainerUseValue(){
-		return Integer.valueOf(this.getMaxContainerUse());
+
+	public int getMaxOrderValue() {
+		try {
+			return Integer.valueOf(this.getMaxOrder());
+		} catch (NumberFormatException e) {
+			LOGGER.warn("bad value for maxWorkOrder: {}", getMaxOrder());
+			return MAX_ORDER;
+		}
 	}
-	
+
+	public int getMaxContainerUseValue() {
+		try {
+			return Integer.valueOf(this.getMaxContainerUse());
+		} catch (NumberFormatException e) {
+			LOGGER.warn("bad value for maxContainerUse: {}", getMaxContainerUse());
+			return MAX_CONTAINERUSE;
+		}
+	}
+
+	/**
+	 * Notice the intent is to return the values that will actually be used.
+	 */
 	@Override
 	public String getParametersDescription() {
-		return String.format("maxOrder: %s , maxOrderDetail: %s , maxWorkInstruction: %s , maxContainerUse: %s", maxOrder, maxOrderDetail, maxWorkInstruction, maxContainerUse);
+		return String.format("maxOrder: %d , maxOrderDetail: %d , maxWorkInstruction: %d , maxContainerUse: %d",
+			getMaxOrder(),
+			getMaxOrderDetailValue(),
+			getMaxWorkInstructionValue(),
+			getMaxContainerUseValue());
 	}
-
 
 }
