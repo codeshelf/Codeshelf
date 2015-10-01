c15b87def12e518f97ddb6d13d69c350d07fcc92
jonranstrom <jonranstrom@gmail.com>
2015-09-30 17:07:05 -0700

    DEV-1172

--- src/main/java/com/codeshelf/service/ParameterSetBeanABC.java
+++ src/main/java/com/codeshelf/service/ParameterSetBeanABC.java
@@ -15,6 +15,12 @@ public abstract class ParameterSetBeanABC {
 		return "need to implement getParametersDescription";
 	}
 
+	/**
+	 * See callers for usage. This is useful only for integer values represented by strings in our groovy extension.
+	 * pass in "fieldname" which should match the String field of the bean. Used only for logging problems in a way useful to user/administrator
+	 * inValue is the value from the getter for the field.
+	 * defaultValue is the value to use if there is a problem
+	 */
 	protected int getCleanValue(String fieldName, String inValue, int defaultValue) {
 		try {
 			int value = Integer.valueOf(inValue);
