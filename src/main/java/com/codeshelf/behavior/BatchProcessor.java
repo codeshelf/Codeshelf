d027b0257d09caa920c84e3298ca4b92e9dc0afa
Paul Monteiro <paul.monteiro@codeshelf.com>
2015-10-07 13:59:16 -0700

    DEV-1197 javadoc batch processor interface

--- src/main/java/com/codeshelf/behavior/BatchProcessor.java
+++ src/main/java/com/codeshelf/behavior/BatchProcessor.java
@@ -2,12 +2,16 @@ package com.codeshelf.behavior;
 
 public interface BatchProcessor {
 
+	/** Return a total for a sense of progress */ 
 	int doSetup() throws Exception;
 
+	/** Return the latest progress (monotonically increasing) */ 
 	int doBatch(int batchCount) throws Exception;
 
+	/** called upon exception or completion */
 	void doTeardown();
 
+	/** set to true to indicate no more batches */
 	boolean isDone();
 	
 	public String toString();
