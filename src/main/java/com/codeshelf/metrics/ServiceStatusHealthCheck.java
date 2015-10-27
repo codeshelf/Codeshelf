b173e53e110a685d3a823381f73c641839221fce
jonranstrom <jonranstrom@gmail.com>
2015-10-27 14:09:39 -0700

    DEV-1260 Service Status health check give better uptime

--- src/main/java/com/codeshelf/metrics/ServiceStatusHealthCheck.java
+++ src/main/java/com/codeshelf/metrics/ServiceStatusHealthCheck.java
@@ -2,6 +2,7 @@ package com.codeshelf.metrics;
 
 import java.lang.management.ManagementFactory;
 import java.lang.management.RuntimeMXBean;
+import java.util.concurrent.TimeUnit;
 
 import com.codeshelf.application.JvmProperties;
 
@@ -15,10 +16,27 @@ public class ServiceStatusHealthCheck extends CodeshelfHealthCheck {
 	protected Result check() throws Exception {
 		RuntimeMXBean runtimeMX = ManagementFactory.getRuntimeMXBean();
 		long jvmUpTimeMillis = runtimeMX.getUptime();
-		String jvmVersion = runtimeMX.getSpecVersion()+"-"+runtimeMX.getVmVersion();
-		
-		return Result.healthy("App v" + JvmProperties.getVersionStringShort() 
-			+ " / JVM " + jvmVersion
-			+ " / uptime " + jvmUpTimeMillis/1000 + "s");
+		String jvmVersion = runtimeMX.getSpecVersion() + "-" + runtimeMX.getVmVersion();
+
+		long millis = jvmUpTimeMillis;
+
+		// Compute a pleasing string
+		long days = TimeUnit.MILLISECONDS.toDays(millis);
+		millis -= TimeUnit.DAYS.toMillis(days);
+		long hours = TimeUnit.MILLISECONDS.toHours(millis);
+		millis -= TimeUnit.HOURS.toMillis(hours);
+		long minutes = TimeUnit.MILLISECONDS.toMinutes(millis);
+		millis -= TimeUnit.MINUTES.toMillis(minutes);
+
+		StringBuilder sb = new StringBuilder(64);
+		sb.append(days);
+		sb.append(" days; ");
+		sb.append(hours);
+		sb.append(" hours; ");
+		sb.append(minutes);
+		sb.append(" minutes ");
+
+		return Result.healthy("App v" + JvmProperties.getVersionStringShort() + " / JVM " + jvmVersion + " / Uptime "
+				+ sb);
 	}
 }
