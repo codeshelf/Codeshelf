7cdcdee76556b3b9fe116094a5e0ca1efd52e820
Ilya Landa <IlyaALanda@gmail.com>
2015-11-13 13:10:12 -0800

    DEV-1218 Facility Metrics

---
+++ src/main/java/com/codeshelf/model/AccumulateDailyMetricsProcessor.java
@@ -0,0 +1,52 @@
+package com.codeshelf.model;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.TimeZone;
+
+import com.codeshelf.behavior.BatchProcessor;
+import com.codeshelf.model.domain.Facility;
+
+public class AccumulateDailyMetricsProcessor implements BatchProcessor{
+	private Facility facility;
+	private boolean done = false;
+
+	public AccumulateDailyMetricsProcessor(Facility facility) {
+		this.facility = facility;
+	}
+	
+	@Override
+	public int doSetup() throws Exception {
+		return 1;
+	}
+
+	@Override
+	public int doBatch(int batchCount) throws Exception {
+		facility = facility.reload();
+		SimpleDateFormat out = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		TimeZone facilityTimeZone = facility.getTimeZone();
+		Calendar cal = Calendar.getInstance(facilityTimeZone);
+		cal.set(Calendar.HOUR_OF_DAY, 0);
+		cal.set(Calendar.MINUTE, 0);
+		cal.set(Calendar.SECOND, 0);
+		cal.set(Calendar.MILLISECOND, 0);
+		cal.add(Calendar.DATE, -1);
+		String dateStr = out.format(cal.getTime());
+		
+		facility.computeMetrics(dateStr, true);
+		
+		done = true;
+		return 1;
+	}
+
+	@Override
+	public void doTeardown() {
+		
+	}
+
+	@Override
+	public boolean isDone() {
+		return done;
+	}
+
+}
